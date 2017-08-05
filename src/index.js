02a063bc7252cb4019fcc6c319f946766eac769b
Aaron Turner <aaron@aaronthedev.com>
2017-08-05 13:01:12 -0700

    Generated application again with create-react-app

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-import { Router, Route, browserHistory } from 'react-router';
-import AMPDocument from './components/amp-document/amp-document';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Shell from './components/shell';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './bootstrap-theme.css'; // Replace with your own bootstrap theme!
-
-/**
- * @see https://github.com/ampproject/amphtml/blob/master/extensions/amp-install-serviceworker/amp-install-serviceworker.md#shell-url-rewrite
- */
-function redirectSWFallbackURL(nextState, replace) {
-  var hash = window.location.hash;
-  if (hash && hash.indexOf('#href=') === 0) {
-    var href = decodeURIComponent(hash.substr(6));
-    replace({pathname: href});
-  }
-}
-
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path='/' component={Shell} onEnter={redirectSWFallbackURL}>
-      <Route path='content/:document' component={
-        props => <AMPDocument src={'/content/' + props.params.document} />
-      } />
-    </Route>
-  </Router>
-), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
